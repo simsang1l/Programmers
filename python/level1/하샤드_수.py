2cdd6f80be6c97ad9704e648a9588e9708bac7d8
simsang1l <simsang1db@gmail.com>
2023-02-27 17:32:30 +0900

    2023.02.27 programmers

---
+++ python/level1/하샤드_수.py
@@ -0,0 +1,17 @@
+def solution(x):
+    answer = True
+    num_lst = [int(i) for i in str(x)]
+
+    if x % sum(num_lst) == 0 :
+        answer = True
+    else :
+        answer = False
+        
+        
+    return answer
+
+## 다른사람 풀이
+
+def solution(n):
+    # n은 하샤드 수 인가요?
+    return n % sum([int(c) for c in str(n)]) == 0
