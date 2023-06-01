ca1672671d05632d03e89f2b8905908ef39a2964
simsang1l <simsang1db@gmail.com>
2023-06-01 15:57:06 +0900

    2023.06.01 programmers

--- python/level2/N개의_최소공배수.py
+++ python/level2/N개의_최소공배수.py
@@ -23,3 +23,17 @@ def solution(arr):
     answer = temp
     
     return answer
+
+# 다른 사람 풀이
+## fractinos는 파이썬에서 유리수를 정확하게 계산하기 위한 모듈
+## 유리수는 두 정수의 비율 또는 분수 형식으로 나타낼 수 있는 수를 말함
+
+from fractions import gcd
+
+
+def solution(num):      
+    answer = num[0]
+    for n in num:
+        answer = n * answer / gcd(n, answer) # 결국 두 수의 최소공배수 구하는 식
+
+    return answer
