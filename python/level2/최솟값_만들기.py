ff5b8016ebf73a7a07d843f4c6563b4162908d55
simsang1l <simsang1db@gmail.com>
2023-06-08 15:33:59 +0900

    속도 개선한 코드 추가

--- python/level2/최솟값_만들기.py
+++ python/level2/최솟값_만들기.py
@@ -12,3 +12,17 @@ def solution(A,B):
         answer += low
 
     return answer
+
+# 이렇게 하는게 더 빠르겠다!
+def solution(A,B):
+    answer = 0
+    A.sort(reverse = True)
+    B.sort()
+    
+    for i in A :
+        low = i * B[0]
+        B.pop(0)
+  
+        answer += low
+
+    return answer
