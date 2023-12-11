61f68b70939604197f12b50a6acdfb378157a609
simsang1l <simsang1db@gmail.com>
2023-12-11 11:10:45 +0900

    2023.12.11 programmers

--- python/level2/튜플.py
+++ python/level2/튜플.py
@@ -20,6 +20,7 @@ def solution(s):
 # 다른 사람 풀이
 def solution(s):
     # 정규표현식을 통해 숫자만 추출
+    # collections의 Counter는 각 원소가 몇 번씩 나오는지가 저장된 객체 얻을 수 있음
     s = Counter(re.findall('\d+', s))
     return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
 
