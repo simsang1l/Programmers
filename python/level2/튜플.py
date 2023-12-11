8645b3a108640226fd194da9d8ee3603dac1c9a2
simsang1l <simsang1db@gmail.com>
2023-12-11 11:10:24 +0900

    2023.12.11 programmers

---
+++ python/level2/튜플.py
@@ -0,0 +1,27 @@
+def solution(s):
+    answer = []
+    dic = {}
+    tu = s.split('},')
+    for i in tu :
+        n = i.replace('{', '').replace('}','')
+        n = n.split(',')
+        
+        dic[len(n)] = n
+    dic = sorted(dic.items(), key = lambda x : x[0])
+    
+    for l, val in dic:
+        for i in val:
+            i = int(i)
+            if i not in answer :
+                answer.append(i)
+            
+    return answer
+
+# 다른 사람 풀이
+def solution(s):
+    # 정규표현식을 통해 숫자만 추출
+    s = Counter(re.findall('\d+', s))
+    return list(map(int, [k for k, v in sorted(s.items(), key=lambda x: x[1], reverse=True)]))
+
+import re
+from collections import Counter
