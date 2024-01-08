64b452ea708f9344d1bf26b78eca0c6d3f0f9141
simsang1l <simsang1db@gmail.com>
2024-01-08 11:54:35 +0900

    2024.01.08 programmers, 재귀 코드 추가

--- python/level1/콜라츠_추측.py
+++ python/level1/콜라츠_추측.py
@@ -1,3 +1,21 @@
+# 재귀로 풀어보기
+def collatz(n, step = 0):
+    if step >= 500:
+        return -1
+    elif n == 1 :
+        return step
+    elif n % 2 == 0:
+        return collatz(n//2, step + 1)
+    else :
+        return collatz(3 * n + 1, step + 1)
+    
+def solution(num):
+    answer = 0
+    
+    answer = collatz(num)
+        
+    return answer
+
 # 문제 설명 그대로 구현하고자 함
 # 최대 500번 수행
 
