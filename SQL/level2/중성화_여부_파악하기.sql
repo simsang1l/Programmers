38737ccff7e6be5ac81d3d9236ff8894bee854dd
심상일 <simsang1db@gmail.com>
2024-03-19 17:45:11 +0900

    2024.03.19 programmers

---
+++ SQL/level2/중성화_여부_파악하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME,
+    case 
+        when SEX_UPON_INTAKE like '%Neutered%' or SEX_UPON_INTAKE like '%Spayed%' then 'O'
+        else 'X'
+    end as `중성화`
+FROM
+    ANIMAL_INS
+;
