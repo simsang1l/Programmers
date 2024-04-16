de2a56e1efd4da57f4658f49b01d361b8e4bd16f
심상일 <simsang1db@gmail.com>
2024-04-16 13:19:51 +0900

    2024.04.16 programmers

---
+++ SQL/level2/자동차_종류_별_특정_옵션이_포함된_자동차_수_구하기.sql
@@ -0,0 +1,22 @@
+SELECT
+    CAR_TYPE,
+    COUNT(*) AS CARS
+FROM
+    CAR_RENTAL_COMPANY_CAR
+WHERE
+    OPTIONS LIKE '%통풍시트%'
+    OR OPTIONS LIKE '%열선시트%'
+    OR OPTIONS LIKE '%가죽시트%'
+GROUP BY
+    CAR_TYPE
+ORDER BY
+    CAR_TYPE
+;
+
+-- 다른 사람 풀이
+-- 정규식을 이용할 수 있음
+SELECT CAR_TYPE, COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS REGEXP '통풍시트|열선시트|가죽시트' -- '통풍시트', '열선시트', '가죽시트' 중 하나 이상의 옵션이 포함
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC;
