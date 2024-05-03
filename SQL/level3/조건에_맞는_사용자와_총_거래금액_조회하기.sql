f2d9878cb2c6e25e131a0ade434059aa009c8a50
심상일 <simsang1db@gmail.com>
2024-05-03 16:09:32 +0900

    2024.05.03 programmers

---
+++ SQL/level3/조건에_맞는_사용자와_총_거래금액_조회하기.sql
@@ -0,0 +1,24 @@
+SELECT
+    USER_ID,
+    NICKNAME,
+    TOTAL_SALES
+FROM
+    (SELECT
+        WRITER_ID ,
+        SUM(PRICE) AS TOTAL_SALES
+     FROM
+        USED_GOODS_BOARD
+     WHERE
+        STATUS = 'DONE'
+     GROUP BY
+        WRITER_ID
+     
+    ) A
+    INNER JOIN
+        USED_GOODS_USER B
+        ON A.WRITER_ID = B.USER_ID
+WHERE
+    TOTAL_SALES >= 700000
+ORDER BY
+    TOTAL_SALES
+;
